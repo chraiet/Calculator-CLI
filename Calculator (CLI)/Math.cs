699f1ed0f45e3870cd5047a1c3713d3edb056841
Mohamed Chraiet <chraiet009@outlook.com>
2017-11-07 10:22:16 -0500

    Added Math class with add and substract operations.

---
+++ Calculator (CLI)/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator__CLI_
+{
+    /*
+     * Class that provides method for doing operations on numbers or get math constants.
+     */
+    class Math
+    {
+        //  Addition operation.
+        public static double Add(double numberA, double numberB)
+        {
+            return numberA + numberB;
+        }
+
+        //  Substraction operation.
+        public static double Substract(double numberA, double numberB)
+        {
+            return numberA - numberB;
+        }
+    }
+}
