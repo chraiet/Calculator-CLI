edc0b1940f13965bb73a22ee24433f85d789ac77
Mohamed Chraiet <chraiet009@outlook.com>
2017-11-07 19:35:48 -0500

    Added add operation and welcome title.

--- Calculator (CLI)/Program.cs
+++ Calculator (CLI)/Program.cs
@@ -10,12 +10,35 @@ namespace Calculator__CLI_
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("******\nWelcome to Calculator (CLI)!\n******\n");
+
             string userInput = "";
             do
             {
-                Console.WriteLine("Choose the operation and press Enter:\n\tQ - Quit");
+                Console.WriteLine("Choose the operation and press Enter:\n\t1 - Add\n\tQ - Quit");
                 userInput = Console.ReadLine();
                 userInput = userInput.ToLower();
+
+                //  Choose the right operation.
+                switch (userInput)
+                {
+                    case "1":
+                    case "add":
+                        Console.Write("Choose first number: ");
+                        string a = Console.ReadLine();
+                        Console.Write("Choose second number: ");
+                        string b = Console.ReadLine();
+                        double numberA;
+                        double.TryParse(a, out numberA);
+                        double numberB;
+                        double.TryParse(b, out numberB);
+                        double resultAdd = Math.Add(numberA, numberB);
+                        Console.WriteLine($"The result of the addition is {resultAdd}");
+                        break;
+                    default:
+                        Console.WriteLine("This operation does not exist. Please try again.");
+                        break;
+                }
             } while (userInput != "q");
         }
     }
